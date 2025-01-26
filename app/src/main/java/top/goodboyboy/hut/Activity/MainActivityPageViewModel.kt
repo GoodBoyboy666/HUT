67093daf662a988a811f94dabe2db36f28f3cffe
GoodBoyboy <me@goodboyboy.top>
2025-01-26 19:15:34 +0800

    增加Tools缓存功能
    优化课表加载
    调整部分Toast弹窗时间

--- app/src/main/java/top/goodboyboy/hut/Activity/MainActivityPageViewModel.kt
+++ app/src/main/java/top/goodboyboy/hut/Activity/MainActivityPageViewModel.kt
@@ -4,6 +4,16 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityPageViewModel:ViewModel() {
     //HutServiceCenter
-    var serviceList=""
+    var serviceListString=""
+
+    //FragmentKb
+    var allItems = List(40) { "" }.toMutableList()
+    var allInfos = List(40) { "N/A" }.toMutableList()
+    var isLoad=false
+    var zhouciSelectedIndex = 0
+    var zhouciSelected = String()
+    var zhouci = mutableListOf<String>()
+    var kbjcmsid = String()
+    var xnxq01id = String()
 
 }
