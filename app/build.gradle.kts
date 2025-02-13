ef8e354669ea05a7796e66c29ff3bf60d7cf66cb
GoodBoyboy <me@goodboyboy.top>
2025-02-13 14:45:23 +0800

    更新版本号
    (cherry picked from commit df24f6d36824e28421eacacfb87786294b9eff1a)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "top.goodboyboy.hut"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.2.6"
+        versionCode = 9
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
